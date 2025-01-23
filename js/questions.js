68f134e8d11f7d4293f3aa2cab6ccb90564dea59
ricky-11254 <erickmuchiri11@gmail.com>
2025-01-23 16:37:36 +0300

    commit

---
+++ js/questions.js
@@ -0,0 +1,105 @@
+
+/** Main Diagnostic Quiz Function **/
+//
+  var questions = [
+    //
+
+    {
+      //1
+      //
+      //b1
+      qType: "Section A: Comprehension & Summary",
+      question: "1. What is the main idea of the passage?",
+      choices: ["A) AI is negatively impacting all aspects of society.",
+                "B) AI is transforming various sectors while also posing certain challenges.",
+                "C) AI has been fully integrated into society with no challenges.",
+                "D) AI’s progress is limited to virtual assistants like Siri and Alexa."],
+      correctAnswer: 1,
+      // audio: "",
+      image: "img/JAMB7_Comprehension_SectionA_PassageI.png",
+      content: "",
+      explanation: ""
+    },
+    {
+      //2
+      //
+      //c2
+      qType: "Section A: Comprehension & Summary",
+      question: "2. In the context of the passage, what does the term '<u>ubiquitous</u>' mean?",
+      choices: ["A) Unnecessary",
+                "B) Rare",
+                "C) Present everywhere",
+                "D) Challenging"],
+      correctAnswer: 2,
+      // audio: "",
+      image: "img/JAMB7_Comprehension_SectionA_PassageI.png",
+      content: "",
+      explanation: ""
+    },
+    {
+      //3
+      //
+      //b1
+      qType: "Section A: Comprehension & Summary",
+      question: "3. Based on the passage, why might AI systems trained on biased data pose a problem?",
+      choices: ["A) They lead to more efficient decision-making.",
+                "B) They can perpetuate and amplify societal biases.",
+                "C) They are unable to process large amounts of data.",
+                "D) They can only be used in specific industries."],
+      correctAnswer: 1,
+      // audio: "",
+      image: "img/JAMB7_Comprehension_SectionA_PassageI.png",
+      content: "",
+      explanation: ""
+    },
+    {
+      //4
+      //
+      //a0
+      qType: "Section A: Comprehension & Summary",
+      question: "4.  What does the phrase '<u>employment displacement</u>' mean in the context of the passage?",
+      choices: ["A) Workers are losing their jobs due to automation.",
+                "B) AI is creating new jobs in technology.",
+                "C) AI is causing employees to be transferred within their workplace.",
+                "D) AI is making work more enjoyable for people."],
+      correctAnswer: 0,
+      // audio: "",
+      image: "img/JAMB7_Comprehension_SectionA_PassageI.png",
+      content: "",
+      explanation: ""
+    },
+    {
+      //5
+      //
+      //b1
+      qType: "Section A: Comprehension & Summary",
+      question: "5. How does AI impact the healthcare sector, according to the passage?",
+      choices: ["A) AI replaces doctors in diagnosing diseases.",
+                "B) AI helps with disease detection and improves record-keeping accuracy.",
+                "C) AI is primarily used for entertainment purposes.",
+                "D) AI only helps in minor administrative tasks."],
+      correctAnswer: 1,
+      // audio: "",
+      image: "img/JAMB7_Comprehension_SectionA_PassageI.png",
+      content: "",
+      explanation: ""
+    },
+/*
+    {
+      //6
+      //
+      //c2
+      qType: "Section A: Comprehension & Summary",
+      question: "Choose the best answer for Number 6.",
+      choices: ["A) loan",
+                "B) balance",
+                "C) service",
+                "D) expense"],
+      correctAnswer: 2,
+      // audio: "",
+      image: "img/JAMB7_Comprehension_SectionA_PassageII.png",
+      content: "",
+      explanation: ""
+    },
+  */
+];
